91d56dc19a1981ea0dbad72fbb3b270e35b6d516
Pedro Peral <pep.90@hotmail.com>
2021-01-04 20:55:18 -0300

    Initial setup.

--- DSDeliver/Startup.cs
+++ DSDeliver/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DSDeliver
 {
@@ -26,6 +19,16 @@ namespace DSDeliver
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddMvcCore()
+                .AddDataAnnotations()
+                .AddCors(x => x.AddPolicy("All",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +44,8 @@ namespace DSDeliver
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("All");
 
             app.UseEndpoints(endpoints =>
             {
